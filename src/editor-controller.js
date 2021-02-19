ac5f1a4831eedb7c149727e084889f67f3f8ae92
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-02-19 09:40:47 +0100

    Add preview of selected/typed color

--- src/editor-controller.js
+++ src/editor-controller.js
@@ -1,11 +1,28 @@
-angular.module("umbraco").controller("ColorSelectorController", function($scope, $element) {
-	$scope.colors = [
-		{ value: '314159', label: '314159' },
-		{ value: 'bada55', label: 'bada55' },
-		{ value: 'ff9090', label: 'ff9090' }
-	]
+angular.module("umbraco").controller("ColorSelectorController", function($scope, angularHelper) {
+	var presets = ['preset1', 'preset2', 'preset3', 'preset4', 'preset5']
 	
-	$scope.didSelectColor = function(color) {
-		$scope.model.value = color.value
+	$scope.colors = []
+	presets.map(function(p) {
+		var preset = validHexColor($scope.model.config[p])
+		if (preset != null) {
+			$scope.colors.push({
+				label: preset,
+				value: preset
+			})
+		}
+	})
+	
+	$scope.didSelectColor = function() {
+		var currentForm = angularHelper.getCurrentForm($scope)
+		currentForm.$setDirty()
+	}
+	
+	function validHexColor(value) {
+		var hexRE = /^#?[a-fA-F0-9]{3,6}$/;
+		if (value && value.match(hexRE)) {
+			return value.replace('#', '')
+		} else {
+			return null
+		}
 	}
 })
